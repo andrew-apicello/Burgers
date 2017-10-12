b2da978413af22dfbedabb413a413f78c60f2373
Andrew C. Apicello <andrew.apicello@gmail.com>
2017-10-12 17:02:10 -0400

    link to Heroku

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+var connnection;
 
-var connection = mysql.createConnection({
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
   port: 3306,
   host: "b4e9xxkxnpu2v96i.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   user: "sz9jwz1yrx3n99p5",
   password: "om81nzx8up7so65i",
-  database: "cat_db"
+  database: "k0zeb0mjoxaeao6k"
 });
+};
 
 // Make connection.
 connection.connect(function(err) {
@@ -18,5 +23,16 @@ connection.connect(function(err) {
   console.log("connected as id " + connection.threadId);
 });
 
+
 // Export connection for our ORM to use.
 module.exports = connection;
+
+
+  port: 3306,
+  host: "b4e9xxkxnpu2v96i.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  user: "sz9jwz1yrx3n99p5",
+  password: "om81nzx8up7so65i",
+  database: "cat_db"
+});
+
+
